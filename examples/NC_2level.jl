3f4a6c829789c28f9a7d0b6f1455011cbbd06bf8
peterrrock2 <peter.r.rock2@gmail.com>
2024-04-03 10:02:50 -0600

    Update example and gitignore

--- examples/NC_2level.jl
+++ examples/NC_2level.jl
@@ -4,19 +4,27 @@ push!(LOAD_PATH, "..");
 using RandomNumbers
 using MultiScaleMapSampler
 
+pctGraphPath = joinpath("..", "test", "test_graphs", "NC_pct21.json")
+precinct_name = "prec_id"
+county_name = "county"
+population_col = "pop2020cen"
 num_dists = 14
 rng_seed = 454190
 pop_dev = 0.02
 gamma = 0 #0 is uniform on forests; 1 is uniform on partitions
 steps = 10
 edge_weights= "connections"
+output_file_path = "./atlas_gamma"*string(gamma)*".jsonl"
 
-pctGraphPath = joinpath("..", "test", "test_graphs", "NC_pct21.json")
-nodeData = Set(["county", "prec_id", "pop2020cen", "area", "border_length"]);
-base_graph = BaseGraph(pctGraphPath, "pop2020cen", inc_node_data=nodeData,
-                       area_col="area", node_border_col="border_length",
-                       edge_perimeter_col="length", edge_weights=edge_weights);
-graph = MultiLevelGraph(base_graph, ["county", "prec_id"]);
+nodeData = Set([precinct_name, county_name, population_col])
+
+base_graph = BaseGraph(
+    pctGraphPath, 
+    population_col, 
+    inc_node_data=nodeData,
+    edge_weights=edge_weights
+);
+graph = MultiLevelGraph(base_graph, [county_name, precinct_name]);
 
 constraints = initialize_constraints()
 add_constraint!(constraints, PopulationConstraint(graph, num_dists, 0.01))
@@ -31,14 +39,18 @@ measure = Measure(gamma)
 # to add elements to the measure
 # push_measure!(measure, get_isoperimetric_score, 0.45)
 
-# output_file_path = "./atlas_gamma"*string(gamma)*".jsonl.bz2"
+# output_file_path = "./atlas_gamma"*string(gamma)*".jsonl"
 # output_file_io = smartOpen(output_file_path, "w")
 # writer = Writer(measure, constraints, partition, output_file_io)
 writer = Writer(measure, constraints, partition, stdout)
 
 
-push_writer!(writer, get_log_spanning_forests)
-push_writer!(writer, get_isoperimetric_scores)
-
-run_metropolis_hastings!(partition, proposal, measure, steps, rng,
-                         writer=writer, output_freq=1);
+run_metropolis_hastings!(
+    partition, 
+    proposal, 
+    measure, 
+    steps, 
+    rng,
+    writer=writer, 
+    output_freq=1
+);
